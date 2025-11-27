6367e2465cdd0f835805ba1de2ee840611aeb75c
Incognito-11 <0incognit11@gmail.com>
2025-11-27 14:55:52 +0530

    layout completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { PageHeaderComponent } from './shared/components/page-header/page-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PageHeaderComponent
   ],
   imports: [
     BrowserModule,
