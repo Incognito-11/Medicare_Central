3b47c7c08c5f883a98a72991553bcb6cc88328a8
Incognito-11 <0incognit11@gmail.com>
2025-11-27 15:16:36 +0530

    doctors initial file creation completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { PageHeaderComponent } from './shared/components/page-header/page-header.component';
+import { DoctorListComponent } from './doctors/components/doctor-list/doctor-list.component';
+import { DoctorDetailsComponent } from './doctors/components/doctor-details/doctor-details.component';
+import { AppointmentFormComponent } from './doctors/components/appointment-form/appointment-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    PageHeaderComponent
+    PageHeaderComponent,
+    DoctorListComponent,
+    DoctorDetailsComponent,
+    AppointmentFormComponent
   ],
   imports: [
     BrowserModule,
